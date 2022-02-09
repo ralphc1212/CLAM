8bd602df3164f6031cf025caae34cd8054ad5a87
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-02-09 16:03:52 +0800

    default

--- processing_scripts/rename_reorganize.py
+++ processing_scripts/rename_reorganize.py
@@ -12,11 +12,11 @@ for name in training_names:
 	type_, post = name.split('_')
 	num, _ = post.split('.')
 
-	num_int = int(num) if type_ == 'normal' else int(num)+160
+	num_int = int(num) - 1 if type_ == 'normal' else int(num)+159
 	print(type_, 'slide_' + str(num_int) + '.' + post_fix)
 
 	rows.append(['patient_' + str(count), 'slide_' + str(num_int), type_, num_int])
-
+	count += 1
 
 # def takeSecond(elem):
     # return elem[3]
