ccd250f5c902e759ee5dd3639167efeca1cec714
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-02-09 15:16:15 +0800

    default

--- processing_scripts/rename_reorganize.py
+++ processing_scripts/rename_reorganize.py
@@ -5,6 +5,11 @@ post_fix = 'h5'
 training_names = os.listdir('/media/hdd/CAMELYON16/feats_resnet50/training/' + post_fix + '_files')
 
 for name in training_names:
-	print(name.split('_'))
-	print(name.split('_')[1].split('.'))
-	print('------')
+	type_, post = name.split('_')
+	num, _ = post.split('.')
+
+	if type_ is 'normal':
+		print('normal', 'slide'+num+'.'+post_fix)
+	else:
+		print(type_, 'slide'+str(int(num)+160)+'.'+post_fix)
+
