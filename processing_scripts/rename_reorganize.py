b9778f71c6a2fa61f63ae14eb37968163dcd0eeb
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-02-09 16:30:35 +0800

    default

--- processing_scripts/rename_reorganize.py
+++ processing_scripts/rename_reorganize.py
@@ -1,25 +1,28 @@
-import os
-import csv
 
-post_fix = 'pt'
 
-training_names = os.listdir('/media/hdd/CAMELYON16/feats_resnet50/training/' + post_fix + '_files')
+################# TRAINING FILES #################
+# import os
+# import csv
 
-rows = []
-# rows = [['case_id', 'slide_id', 'label']]
+# post_fix = 'pt'
 
-count = 0
-for name in training_names:
-	type_, post = name.split('_')
-	num, _ = post.split('.')
+# training_names = os.listdir('/media/hdd/CAMELYON16/feats_resnet50/training/' + post_fix + '_files')
 
-	num_int = int(num) - 1 if type_ == 'normal' else int(num)+159
-	# print(type_, 'slide_' + str(num_int) + '.' + post_fix)
+# rows = []
+# # rows = [['case_id', 'slide_id', 'label']]
 
-	rows.append([num_int, 'slide_' + str(num_int), type_])
-	count += 1
-	os.rename('/media/hdd/CAMELYON16/feats_resnet50/training/' + post_fix + '_files/' + name,
-	 '/media/hdd/CAMELYON16/feats_resnet50/training/' + post_fix + '_files/slide_' + str(num_int) + '.' + post_fix)
+# count = 0
+# for name in training_names:
+# 	type_, post = name.split('_')
+# 	num, _ = post.split('.')
+
+# 	num_int = int(num) - 1 if type_ == 'normal' else int(num)+159
+# 	# print(type_, 'slide_' + str(num_int) + '.' + post_fix)
+
+# 	rows.append([num_int, 'slide_' + str(num_int), type_])
+# 	count += 1
+# 	os.rename('/media/hdd/CAMELYON16/feats_resnet50/training/' + post_fix + '_files/' + name,
+# 	 '/media/hdd/CAMELYON16/feats_resnet50/training/' + post_fix + '_files/slide_' + str(num_int) + '.' + post_fix)
 
 # def takeSecond(elem):
     # return elem[3]
@@ -34,3 +37,14 @@ for name in training_names:
 # with open('training_cases.csv', 'w') as file:
 # 	writer = csv.writer(file)
 # 	writer.writerows(rows)
+
+
+################# TESTING FILES #################
+import os
+import csv
+
+with open('/media/hdd/CAMELYON16/testing_patched/images/process_list_autogen.csv', 'r') as file:
+	reader = csv.reader(file)
+
+for row in reader:
+	print(row)
