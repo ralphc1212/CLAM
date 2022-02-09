25ba8d7f26116ff40db18cddf53900699696bb99
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-02-09 13:19:01 +0800

    default

--- processing_scripts/write_cmln_csv.py
+++ processing_scripts/write_cmln_csv.py
@@ -3,5 +3,8 @@ import os
 normal_names = os.listdir('/media/hdd/CAMELYON16/training_patched/normal/patches')
 tumor_names = os.listdir('/media/hdd/CAMELYON16/training_patched/tumor/patches')
 
+normal_names = [name[:-3] for name in normal_names]
+tumor_names = [name[:-3] for name in tumor_names]
+
 print(normal_names)
 print(tumor_names)
