2c90a8affad242a102bbef726ecee4221b0d196a
ralphc <ralphc@ralphcdeMacBook-Air.local>
2022-03-17 20:15:07 +0800

    default

--- models/model_hattn.py
+++ models/model_hattn.py
@@ -98,7 +98,7 @@ class MIL_hattn(nn.Module):
         self.attention_net = self.attention_net.to(device)
         self.classifiers = self.classifiers.to(device)
         self.temperature = self.temperature.to(device)
-        self.attn_thres_r = self.attn_thres_r.to(device)
+        self.attn_thres_r.to(device)
 
     def forward(self, h, return_features=False):
         device = h.device
