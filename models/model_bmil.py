373d64832fdd3a1bb962f6401ba993a816af7242
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-07-13 18:11:25 -0400

    default

--- models/model_bmil.py
+++ models/model_bmil.py
@@ -303,6 +303,9 @@ class probabilistic_MIL_Bayes_enc(nn.Module):
         postr_alpha = torch.transpose(postr_alpha, 1, 0)  # KxN
         prior_alpha = F.softplus(torch.transpose(prior_alpha, 1, 0))  # KxN
 
+        print(torch.max(torch.softmax(postr_alpha, dim=1)))
+        print(torch.min(torch.softmax(postr_alpha, dim=1)))
+
         print('before: ', postr_alpha)
         postr_alpha = slide_label * self.conc_pos * torch.softmax(postr_alpha, dim=1)  
         + (1 - slide_label) * self.conc_neg * torch.softmax(postr_alpha / 2., dim=1)
