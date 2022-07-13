30511e0694087a7ce6a3443a35cdd5d421e497c4
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-07-13 17:37:11 -0400

    default

--- models/model_bmil.py
+++ models/model_bmil.py
@@ -294,14 +294,14 @@ class probabilistic_MIL_Bayes_enc(nn.Module):
         device = h.device
         #*-*# A, h = self.attention_net(h)  # NxK 
 
-        postr_alpha = self.postr_net(h)
-        prior_alpha = self.prior_net(h)
+        postr_alpha, h = self.postr_net(h)
+        prior_alpha, _ = self.prior_net(h)
 
         # if negative, all patches should be checked with equal probabilities.
         # postr_alpha *= torch.exp(slide_label * torch.tensor([conc_expo]))
 
-        print(postr_alpha)
-        print(prior_alpha)
+        print(postr_alpha.shape)
+        print(prior_alpha.shape)
 
         postr_alpha = slide_label * self.conc_pos * torch.softmax(postr_alpha)  
         + (1 - slide_label) * self.conc_neg * torch.softmax(postr_alpha / 10.)
