cbb57d3a3a9f9b894eb3a172e59a648ec5cd1140
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-07-17 14:39:24 -0400

    default

--- models/model_bmil.py
+++ models/model_bmil.py
@@ -330,7 +330,7 @@ class probabilistic_MIL_Bayes_enc(nn.Module):
         if slide_label == 1:
             postr_alpha = (self.sf_pos * torch.softmax(postr_alpha / 0.1, dim=1))
         else:
-            postr_alpha = (self.sf_neg * torch.softmax(postr_alpha / 5., dim=1)).clamp(max=0.95)
+            postr_alpha = (self.sf_neg * torch.softmax(postr_alpha / 5., dim=1)).clamp(max=0.99)
 
         # print(1. - slide_label)
         # print('after: ', postr_alpha)
