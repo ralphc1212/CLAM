e4fb35438c38acf32ceb844a4435f491679f9956
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-07-27 17:08:43 -0400

    default

--- main.py
+++ main.py
@@ -172,6 +172,7 @@ if args.task == 'task_1_tumor_vs_normal':
                             patient_strat=False,
                             ignore=[])
     if 'convis' in args.model_type:
+        print('load from h5 !!!!!!!')
         dataset.load_from_h5(True)
 
 elif args.task == 'task_2_tumor_subtyping':
