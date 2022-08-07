c9409a2fb5f8087ed03c8f9854a2f3aefc05778b
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-08-07 14:32:27 -0400

    default

--- models/linear_vdo.py
+++ models/linear_vdo.py
@@ -97,8 +97,7 @@ class LinearVDO(nn.Module):
         element_wise_kl = -.5 * torch.log(1 + 1. / (torch.exp(log_alp))) \
                           + k1 * torch.exp(-(k2 + k3 * log_alp) ** 2)
 
-        print(element_wise_kl.shape)
-        sum_kl = element_wise_kl.mean(dim=(1,))
+        # sum_kl = element_wise_kl.mean(dim=(1,))
 
         return - sum_kl.sum()
         # return -torch.mean(minus_kl)
