0e4f47a594d8d0b61aaf7eba8078a1112de89c10
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-08-07 13:41:52 -0400

    default

--- models/model_bmil.py
+++ models/model_bmil.py
@@ -727,6 +727,8 @@ class probabilistic_MIL_Bayes_spvis(nn.Module):
         gaus_samples = self.reparameterize(mu, logvar)
         A = F.sigmoid(gaus_samples)
         # M = A.mul(h).sum(dim=(2, 3)) / A.sum()
+        print(A.shape)
+        print(h.shape)
         M = A.mul(h).sum(dim=(1, 2)) / A.sum()
 
         # print(M.shape)
