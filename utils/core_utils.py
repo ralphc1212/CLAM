9213ad7d3964f48a4859ccad5c0238420bd4e5d9
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-08-07 19:31:29 -0400

    default

--- utils/core_utils.py
+++ utils/core_utils.py
@@ -351,7 +351,8 @@ def train_loop(epoch, model, loader, optimizer, n_classes, writer = None, loss_f
         if bayes_args:
             
             kl_model = bayes_args[0](model)
-
+            print(bayes_args)
+            exit()
             if 'enc' in bayes_args:
                 # loss += bayes_args[1] * kl_div[0]    
                 kl_data = kl_div[0]
