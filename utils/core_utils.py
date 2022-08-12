da83256772f2b8224d0fcd053267980eb45398ae
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-08-12 13:25:48 -0400

    default

--- utils/core_utils.py
+++ utils/core_utils.py
@@ -343,8 +343,7 @@ def train_loop(epoch, model, loader, optimizer, n_classes, writer = None, loss_f
         data, label = data.to(device), label.to(device)
 
         print(bayes_args)
-        if ('enc' or 'spvis') in bayes_args:
-            print('in')
+        if 'enc' in bayes_args or 'spvis' in bayes_args:
             logits, Y_prob, Y_hat, kl_div, _, _ = model(data, slide_label=label)
         else:
             logits, Y_prob, Y_hat, _, _ = model(data)
