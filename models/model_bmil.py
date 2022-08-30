8d61df27d41677e184de1373b6a66ad86deb43f7
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-08-30 15:16:43 -0400

    default

--- models/model_bmil.py
+++ models/model_bmil.py
@@ -711,14 +711,14 @@ class probabilistic_MIL_Bayes_crf(nn.Module):
 
     def full_crf_learning(self, samples):
         # use a learnable Gaussian kernel
-        Q = (1. / torch.exp( samples ).sum()) * torch.exp( samples )
+        # Q = (1. / torch.exp( samples ).sum()) * torch.exp( samples )
         unary = samples
 
         pad = int((self.kernel_size - 1) / 2)
         A = F.pad(samples, (pad, pad, pad, pad), mode='constant', value=0)
         W = self._compute_conv_param()
-        A = Q * F.conv2d(A, weight=W) * self.message_param
-        A = unary - A
+        A = F.conv2d(A, weight=W) * self.message_param
+        A = unary + A
         return A
 
     def crf_approx(self, params):
