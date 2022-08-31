c128219efc0a24cb9b0e9d18c56211bbcffcf523
ralphc1212 <55773420+ralphc1212@users.noreply.github.com>
2022-08-30 20:40:56 -0400

    default

--- models/model_bmil.py
+++ models/model_bmil.py
@@ -785,7 +785,7 @@ class probabilistic_MIL_Bayes_crf(nn.Module):
         # M = A.mul(h).sum(dim=(2, 3)) / A.sum()
 
         # Gaussian smoothing afterwards
-        nMCSamples = 1
+        nMCSamples = 4
         A = 0
         for i in range(nMCSamples):
             gaus_samples = self.reparameterize(mu, logvar)
