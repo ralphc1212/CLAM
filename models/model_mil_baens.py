a33583927906457c9c85be8b751200a733147f14
Ralphen CUI <ralphc@Ralphens-MacBook-Air.local>
2023-03-22 14:15:57 -0400

    default

--- models/model_mil_baens.py
+++ models/model_mil_baens.py
@@ -4,8 +4,9 @@ import torch.nn.functional as F
 from utils.utils import initialize_weights
 import numpy as np
 
+
 class dense_baens(nn.Module):
-    def __init__(self, N=4, D1=3, D2=2):
+    def __init__(self, N = 4, D1 = 3, D2 = 2):
         super(dense_baens, self).__init__()
 
         self.N = N
@@ -49,7 +50,6 @@ class Attn_Net_Gated(nn.Module):
         self.attention_b = nn.Sequential(*self.attention_b)
 
         self.attention_c = dense_baens(4, D, n_classes)
-
 
     def forward(self, x):
         x_ = x
@@ -97,9 +97,6 @@ class MIL_fc_baens(nn.Module):
 
         A, h = self.attention_net(h)
 
-        print(h.shape)
-        exit()
-
         A = torch.transpose(A, 1, 0)  # KxN
 
         # A = F.softmax(A, dim=1)  # softmax over N
@@ -122,6 +119,117 @@ class MIL_fc_baens(nn.Module):
             top_features = torch.index_select(h, dim=0, index=top_instance_idx)
             results_dict.update({'features': top_features})
         return top_instance, Y_prob, Y_hat, y_probs, results_dict
+
+
+# with prior distribution
+# distance measure: MMD
+# 
+class MIL_fc_baens_wpr(nn.Module):
+    def __init__(self, gate = True, size_arg = "small", dropout = False, n_classes=2, top_k=1):
+        super(MIL_fc_baens_wpr, self).__init__()
+        self.size_dict = {"small": [1024, 512, 256], "big": [1024, 512, 384]}
+        size = self.size_dict[size_arg]
+        fc = [nn.Linear(size[0], size[1]), nn.ReLU()]
+
+        if dropout:
+            fc.append(nn.Dropout(0.25))
+        if gate:
+            attention_net = Attn_Net_Gated(L = size[1], D = size[2], dropout = dropout, n_classes = 1)
+        else:
+            attention_net = Attn_Net(L = size[1], D = size[2], dropout = dropout, n_classes = 1)
+
+        fc.append(attention_net)
+        self.attention_net = nn.Sequential(*fc)
+        self.classifiers = nn.Linear(size[1], n_classes)
+        self.n_classes = n_classes
+        self.print_sample_trigger = False
+        self.num_samples = 16
+        self.temperature = torch.tensor([1.0])
+
+        codebook_size = 256
+
+        # self.code_book = nn.Parameter(torch.randn(codebook_size, size[1]))
+        self.code_book = nn.Embedding(codebook_size, size[1])
+
+        initialize_weights(self)
+        self.top_k=top_k
+
+    def relocate(self):
+        device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.attention_net = self.attention_net.to(device)
+        self.classifiers = self.classifiers.to(device)
+        self.temperature = self.temperature.to(device)
+
+    # def prior(self):
+    #     # how to encode doctor knowledge?
+    #     pass
+
+    # def codebook(self, cb_size):
+    #     pass
+
+    # def MMD(self):
+    #     pass
+
+    def vector_quantization(slide_embedding):
+        dist = torch.sum(slide_embedding ** 2, dim=1, keep_dim=True) + \
+               torch.sum(self.code_book.weight ** 2, dim=1) + \
+               2 * torch.matmul(slide_embedding, self.code_book.weight.t())
+
+        encoding_inds = torch.argmin(dist, dim=1).unsqueeze(1)
+
+        device = slide_embedding.device
+        encoding_one_hot = torch.zeros(encoding_inds.size(0), self.K, device=device)
+        encoding_one_hot.scatter_(1, encoding_inds, 1)  # [BHW x K]
+
+        quantized_latents = torch.matmul(encoding_one_hot, self.embedding.weight)  # [BHW, D]
+        quantized_latents = quantized_latents.view(latents_shape)  # [B x H x W x D]
+
+        # Compute the VQ Losses
+        commitment_loss = F.mse_loss(quantized_latents.detach(), latents)
+        embedding_loss = F.mse_loss(quantized_latents, latents.detach())
+
+        vq_loss = commitment_loss * self.beta + embedding_loss
+
+        # Add the residue back to the latents
+        quantized_latents = latents + (quantized_latents - latents).detach()
+
+        return
+
+    def forward(self, h, return_features=False, slide_label=None, validation=False):
+        device = h.device
+        #*-*# A, h = self.attention_net(h) # NxK
+
+        A, h = self.attention_net(h)
+
+        A = torch.transpose(A, 1, 0)  # KxN
+
+        # A = F.softmax(A, dim=1)  # softmax over N
+        # M = torch.mm(A, h)
+
+        A = F.sigmoid(A)
+        M = torch.mm(A, h) / A.sum()
+
+        vq_loss, M_code = vector_quantization(M)
+
+        # M = torch.mm(A, h)
+
+        logits = self.classifiers(M)
+
+        y_probs = F.softmax(logits, dim = 1)
+        top_instance_idx = torch.topk(y_probs[:, 1], self.top_k, dim=0)[1].view(1,)
+        top_instance = torch.index_select(logits, dim=0, index=top_instance_idx)
+        Y_hat = torch.topk(top_instance, 1, dim = 1)[1]
+        Y_prob = F.softmax(top_instance, dim = 1)
+        results_dict = {}
+
+        if return_features:
+            top_features = torch.index_select(h, dim=0, index=top_instance_idx)
+            results_dict.update({'features': top_features})
+
+        if not validation:
+            return top_instance, Y_prob, Y_hat, kl_div, y_probs, A
+        else:
+            return top_instance, Y_prob, Y_hat, y_probs, A
 
 
 class MIL_fc_baens_pr(nn.Module):
